ca4f6a343e0f3259e440b29789f4d77e8c168735
John Cant <a.johncant@gmail.com>
2012-11-30 16:57:36 +0000

    Removed annoying output from rake task loading

--- lib/mailman-rails/railtie.rb
+++ lib/mailman-rails/railtie.rb
@@ -4,7 +4,7 @@ module Mailman
   module Rails
     class Railtie < ::Rails::Railtie
       rake_tasks do
-        Dir[File.expand_path('../tasks/*.rake', __FILE__)].each { |f| puts f ; load f }
+        Dir[File.expand_path('../tasks/*.rake', __FILE__)].each { |f| load f }
       end
     end
   end
